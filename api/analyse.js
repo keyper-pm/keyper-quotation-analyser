682275c5229df11b8e8fe9f7d8af34474aa64089
keyper-pm <vasim@realkeyper.com>
2026-03-06 19:27:43 +0400

    Fix 3

--- api/analyse.js
+++ api/analyse.js
@@ -1,7 +1,4 @@
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST');
-  
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
 
   try {
